350604764d08ed79640e6cb0882818524965938d
Fatma Birel <fatmabrl11@gmail.com>
2024-10-02 13:30:53 +0300

    project folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'library';
